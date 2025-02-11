1711acf036843e71ec7cc889c775d0a8c1a3bd1f
Allison Chartrand <achartra@gpu004.atstor.adapt.nccs.nasa.gov>
2025-02-11 14:48:03 -0500

    todays wisely weighted runs

--- pinnbatch_par1.sh
+++ pinnbatch_par1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=pinn2025-Feb_10_1G        # Job name (or -J my_job_name)
+#SBATCH --job-name=pinn2025-Feb_11_1G        # Job name (or -J my_job_name)
 #SBATCH --output=output.%J           # Standard output file (or -o output.%j for job number to be appended)
 #SBATCH --error=error.%J             # Standard error file (or -e error.%j for job number to be appended)
-#SBATCH --gpus=4		      # Number of GPUs required for job (or -G1)
+#SBATCH --gpus=3		      # Number of GPUs required for job (or -G1)
 #SBATCH --gpus-per-task=1
-#SBATCH --nodes=4                    # Number of nodes (or -N1)
+#SBATCH --nodes=3                    # Number of nodes (or -N1)
 #SBATCH --mem=128000		      # Minimum amount of memory to allocate per node
-#SBATCH --ntasks=4           # Number of tasks per node (or -n1)
+#SBATCH --ntasks=3           # Number of tasks per node (or -n1)
 #SBATCH --cpus-per-task=10             # Number of CPU cores per task (or -c1) 
 #SBATCH --time=5:00:00                # Maximum runtime (D-HH:MM:SS)
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT_80               # Send email at job completion
@@ -20,5 +20,4 @@ conda activate PINNICLEenv
 #Run tasks in parallel with ‘&’ and 'wait'
 srun -G1 -n1 -N1 python Ryder_MOLHO_H_short1.py & 
 srun -G1 -n1 -N1 python Ryder_MOLHO_H_short2.py &
-srun -G1 -n1 -N1 python Ryder_MOLHO_H_short3.py & 
-srun -G1 -n1 -N1 python Ryder_MOLHO_H_short4.py & wait
+srun -G1 -n1 -N1 python Ryder_MOLHO_H_short3.py & wait
--- pinnbatch_par2.sh
+++ pinnbatch_par2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=pinn2025-Feb_6_6G        # Job name (or -J my_job_name)
+# -*- coding: utf-8 -*-
+#SBATCH --job-name=pinn2025-Feb_11_4G        # Job name (or -J my_job_name)
 #SBATCH --output=output.%J           # Standard output file (or -o output.%j for job number to be appended)
 #SBATCH --error=error.%J             # Standard error file (or -e error.%j for job number to be appended)
 #SBATCH --gpus=4		      # Number of GPUs required for job (or -G1)
@@ -8,7 +9,7 @@
 #SBATCH --mem=128000		      # Minimum amount of memory to allocate per node
 #SBATCH --ntasks=4           # Number of tasks per node (or -n1)
 #SBATCH --cpus-per-task=10             # Number of CPU cores per task (or -c1) 
-#SBATCH --time=30:00:00                # Maximum runtime (D-HH:MM:SS)
+#SBATCH --time=5:00:00                # Maximum runtime (D-HH:MM:SS)
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT_80               # Send email at job completion
 #SBATCH --mail-user=allison.chartrand@nasa.gov    # Email address for notifications
 #SBATCH --export=ALL		      # identify which env variables are propagated to launched application
@@ -18,7 +19,7 @@ module load miniforge
 conda activate PINNICLEenv
 #Run the same task
 #Run tasks in parallel with ‘&’ and 'wait'
-srun -G1 -n1 -N1 python Ryder_MOLHO_H_long5.py & 
-srun -G1 -n1 -N1 python Ryder_MOLHO_H_long6.py & 
-srun -G1 -n1 -N1 python Ryder_MOLHO_H_long7.py & 
-srun -G1 -n1 -N1 python Ryder_MOLHO_H_long8.py & wait
+srun -G1 -n1 -N1 python Ryder_MOLHO_H_short4.py & 
+srun -G1 -n1 -N1 python Ryder_MOLHO_H_short5.py &
+srun -G1 -n1 -N1 python Ryder_MOLHO_H_short6.py & 
+srun -G1 -n1 -N1 python Ryder_MOLHO_H_short7.py & wait
